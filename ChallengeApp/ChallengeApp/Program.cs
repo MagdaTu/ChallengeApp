6774984f97af9a644b4b23157b76294675506e50
MagdaTu <magrzan@o2.pl>
2023-09-27 19:30:07 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic.FileIO;
+using System.ComponentModel.Design;
+
+var name = "Magda";
+var sex = "female";
+var age = 29;
+if (age < 30)
+    if (sex == "female" && age < 30)
+    {
+    Console.WriteLine("KOBIETA PONIŻEJ 30 LAT");
+    }
+    else
+    {
+    Console.WriteLine("MĘŻCZYZNA PONIŻEJ 30 LAT");
+    }
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("EWA LAT 33");
+}
+else if (sex != "female" && age < 18)
+{
+    Console.WriteLine("MĘŻCZYZNA NIEPEŁNOLETNI");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
